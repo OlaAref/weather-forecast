73ff6d3d1e0cc91d660c63a8b650565cfdd7a75c
OlaAref <olaaref1@gmail.com>
2025-03-19 13:51:13 +0200

    :recycle: Convert location record to class

--- libraries/common-lib/src/main/java/com/olaaref/weather/commonlib/model/Location.java
+++ libraries/common-lib/src/main/java/com/olaaref/weather/commonlib/model/Location.java
@@ -2,52 +2,57 @@ package com.olaaref.weather.commonlib.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.olaaref.weather.commonlib.dto.LocationDto;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "LOCATIONS")
-public record Location(
-        @Id
-        @Column(name = "CODE", length = 2, nullable = false, unique = true)
-        String code,
+public class Location {
+    @Id
+    @Column(name = "CODE", length = 12, nullable = false, unique = true)
+    private String code;
 
-        @Column(name = "COUNTRY_CODE", length = 2, nullable = false)
-        String countryCode,
+    @Column(name = "COUNTRY_CODE", length = 2, nullable = false)
+    private String countryCode;
 
-        @Column(name = "COUNTRY_NAME", length = 64, nullable = false)
-        String countryName,
+    @Column(name = "COUNTRY_NAME", length = 128, nullable = false)
+    private String countryName;
 
-        @Column(name = "REGION_NAME", length = 128)
-        String regionName,
+    @Column(name = "REGION_NAME", length = 128)
+    private String regionName;
 
-        @Column(name = "CITY_NAME", length = 128, nullable = false)
-        String cityName,
+    @Column(name = "CITY_NAME", length = 128, nullable = false)
+    private String cityName;
 
-        @Column(name = "LATITUDE")
-        double latitude,
+    @Column(name = "LATITUDE")
+    private double latitude;
 
-        @Column(name = "LONGITUDE")
-        double longitude,
+    @Column(name = "LONGITUDE")
+    private double longitude;
 
-        @Column(name = "ZIP_CODE", length = 30)
-        String zipCode,
+    @Column(name = "ZIP_CODE", length = 30)
+    private String zipCode;
 
-        @Column(name = "TIME_ZONE", length = 8)
-        String timeZone,
+    @Column(name = "TIME_ZONE", length = 30)
+    private String timeZone;
 
-        @Column(name = "ENABLED")
-        boolean enabled,
+    @Column(name = "ENABLED")
+    private boolean enabled;
 
-        @JsonIgnore
-        @Column(name = "TRASHED")
-        boolean trashed
-) {
-    //add a default constructor to make JPA happy
-    public Location() {
-        this(null, null, null, null, null, 0.0, 0.0, null, null, true, false);
-    }
+    @JsonIgnore
+    @Column(name = "TRASHED")
+    private boolean trashed;
 
     public LocationDto toLocationDto() {
-        return new LocationDto(code(), countryCode(), countryName(), regionName(), cityName(), latitude(), longitude(), zipCode(), timeZone(), enabled(), trashed());
+        return new LocationDto(code, countryCode, countryName, regionName, cityName, latitude, longitude, zipCode, timeZone, enabled, trashed);
     }
+
 }
