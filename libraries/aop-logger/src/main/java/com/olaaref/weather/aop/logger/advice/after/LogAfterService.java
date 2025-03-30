5b4bd29f431235f61266eeea2d15a2274d962405
OlaAref <olaaref1@gmail.com>
2025-03-30 17:04:36 +0200

    :sparkles: Add JavaDoc for log after and log around services

--- libraries/aop-logger/src/main/java/com/olaaref/weather/aop/logger/advice/after/LogAfterService.java
+++ libraries/aop-logger/src/main/java/com/olaaref/weather/aop/logger/advice/after/LogAfterService.java
@@ -18,6 +18,20 @@ import java.time.Duration;
 
 import static com.olaaref.weather.aop.logger.util.LoggerUtil.isExceptionIgnored;
 
+/**
+ * Service class responsible for handling logging after method execution in the AOP logging framework.
+ * <p>
+ * This service processes {@link LogAfter} annotations and generates appropriate log messages
+ * based on the method execution result (normal return or exception thrown). It supports
+ * customizable log messages through templates and variable interpolation.
+ * <p>
+ * The service integrates with various string supplier registrars to populate context information
+ * in log messages, such as method details, return values, and exception information.
+ *
+ * @see LogAfter
+ * @see com.olaaref.weather.aop.logger.util.LoggerUtil
+ * @see com.olaaref.weather.aop.logger.template.interpolation.substitutor.StringSubstitutor
+ */
 public class LogAfterService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogAfterService.class);
@@ -36,10 +50,30 @@ public class LogAfterService {
 
     private final AopLoggersProperties aopLoggersProperties;
 
+    /**
+     * Constructs a new LogAfterService with the specified properties.
+     *
+     * @param aopLoggersProperties The properties containing default configuration for AOP logging
+     */
     public LogAfterService(AopLoggersProperties aopLoggersProperties) {
         this.aopLoggersProperties = aopLoggersProperties;
     }
 
+    /**
+     * Logs information after a method has been executed, based on the {@link LogAfter} annotation.
+     * <p>
+     * This method handles both normal method execution (with a return value) and exceptional
+     * method execution (with an exception). It determines the appropriate logging level and
+     * message template based on the execution outcome and annotation settings.
+     * <p>
+     * If logging is disabled for the determined level or if the exception is configured to be
+     * ignored, only the elapsed time will be logged at debug level.
+     *
+     * @param joinPoint The JoinPoint representing the intercepted method
+     * @param logAfterAnnotation The LogAfter annotation containing logging configuration
+     * @param returnValue The value returned by the method (null if an exception was thrown)
+     * @param exception The exception thrown by the method (null if method completed normally)
+     */
     public void logAfter(JoinPoint joinPoint, LogAfter logAfterAnnotation, Object returnValue, Throwable exception) {
         long startTime = System.nanoTime();
 
@@ -60,6 +94,22 @@ public class LogAfterService {
         logElapsed(startTime);
     }
 
+    /**
+     * Logs the appropriate message based on method execution outcome.
+     * <p>
+     * This method registers the necessary string suppliers based on the execution context
+     * (normal return or exception) and generates the log message using the appropriate template.
+     * If the method is configured to be ignored, no logging will occur.
+     *
+     * @param joinPoint The JoinPoint representing the intercepted method
+     * @param level The logging level to use
+     * @param logAfterAnnotation The LogAfter annotation containing logging configuration
+     * @param logger The logger to use for logging
+     * @param stringLookup The string lookup for variable interpolation
+     * @param returnValue The value returned by the method (null if an exception was thrown)
+     * @param exception The exception thrown by the method (null if method completed normally)
+     * @param isAfterThrowing Flag indicating whether the method threw an exception
+     */
     private void logMessage(
             JoinPoint joinPoint,
             Level level,
@@ -87,19 +137,53 @@ public class LogAfterService {
 
     }
 
+    /**
+     * Determines the message template to use based on annotation and execution context.
+     * <p>
+     * If the annotation provides a non-empty message, it will be used. Otherwise,
+     * the default message from properties will be used based on whether an exception was thrown.
+     *
+     * @param annotationMessage The message specified in the annotation
+     * @param isAfterThrowing Flag indicating whether the method threw an exception
+     * @return The message template to use
+     */
     private String getMessageTemplate(String annotationMessage, boolean isAfterThrowing) {
         if(!annotationMessage.isEmpty()) return annotationMessage;
         return isAfterThrowing ? aopLoggersProperties.getExitedAbnormallyMessage() : aopLoggersProperties.getExitedMessage();
     }
 
+    /**
+     * Logs the elapsed time for the logging operation itself at debug level.
+     * <p>
+     * This is used to track the performance impact of the logging framework.
+     *
+     * @param startTime The start time in nanoseconds
+     */
     private void logElapsed(long startTime) {
         LOGGER.debug("[logAfter] elapsed [{}]", Duration.ofNanos(System.nanoTime() - startTime));
     }
 
+    /**
+     * Checks if logging is disabled for the specified level.
+     *
+     * @param logger The logger to check
+     * @param level The logging level to check
+     * @return true if logging is disabled, false otherwise
+     */
     private boolean isLoggingLevelDisabled(Logger logger, Level level) {
         return !LoggerUtil.isEnabled(logger, level);
     }
 
+    /**
+     * Determines the appropriate logging level based on annotation and execution context.
+     * <p>
+     * If the annotation specifies a non-default level, it will be used. Otherwise,
+     * the default level from properties will be used based on whether an exception was thrown.
+     *
+     * @param annotationLevel The level specified in the annotation
+     * @param isAfterThrowing Flag indicating whether the method threw an exception
+     * @return The logging level to use
+     */
     private Level getLoggingLevel(Level annotationLevel, boolean isAfterThrowing) {
         if(annotationLevel != Level.DEFAULT) return annotationLevel;
         return isAfterThrowing ? aopLoggersProperties.getExitedAbnormallyLevel() : aopLoggersProperties.getExitedLevel();
--- libraries/aop-logger/src/main/java/com/olaaref/weather/aop/logger/advice/around/LogAroundService.java
+++ libraries/aop-logger/src/main/java/com/olaaref/weather/aop/logger/advice/around/LogAroundService.java
@@ -16,6 +16,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.olaaref.weather.aop.logger.util.LoggerUtil.isExceptionIgnored;
 
+/**
+ * Service class responsible for handling logging around method execution in the AOP logging framework.
+ * <p>
+ * This service processes {@link LogAround} annotations and generates appropriate log messages
+ * at different stages of method execution:
+ * <ul>
+ *   <li>Before method execution (entering)</li>
+ *   <li>After successful method execution (exited)</li>
+ *   <li>After method execution with exception (exited abnormally)</li>
+ *   <li>Elapsed time for method execution</li>
+ * </ul>
+ * <p>
+ * The service integrates with various string supplier registrars to populate context information
+ * in log messages, such as method details, arguments, return values, exceptions, and execution time.
+ * <p>
+ * Unlike {@code LogBeforeService} and {@code LogAfterService}, this service uses a {@link ProceedingJoinPoint}
+ * to control the execution flow of the intercepted method, allowing for comprehensive logging
+ * throughout the entire method execution lifecycle.
+ *
+ * @see LogAround
+ * @see com.olaaref.weather.aop.logger.util.LoggerUtil
+ * @see com.olaaref.weather.aop.logger.template.interpolation.substitutor.StringSubstitutor
+ */
 public class LogAroundService {
 
     @Autowired
@@ -35,11 +58,33 @@ public class LogAroundService {
 
     private final AopLoggersProperties aopLoggersProperties;
 
+    /**
+     * Constructs a new LogAroundService with the specified properties.
+     *
+     * @param aopLoggersProperties The properties containing default configuration for AOP logging
+     */
     public LogAroundService(AopLoggersProperties aopLoggersProperties) {
         this.aopLoggersProperties = aopLoggersProperties;
     }
 
-
+    /**
+     * Main method that handles logging around method execution based on the {@link LogAround} annotation.
+     * <p>
+     * This method implements the complete around advice lifecycle:
+     * <ol>
+     *   <li>Logs the entering message before method execution</li>
+     *   <li>Executes the intercepted method</li>
+     *   <li>Logs the exited message after successful method execution</li>
+     *   <li>Logs the elapsed time for method execution</li>
+     *   <li>Logs the exited abnormally message if an exception occurs</li>
+     *   <li>Re-throws any exceptions to maintain the original method behavior</li>
+     * </ol>
+     *
+     * @param joinPoint The ProceedingJoinPoint representing the intercepted method
+     * @param logAround The LogAround annotation containing logging configuration
+     * @return The value returned by the intercepted method
+     * @throws Throwable Any exception thrown by the intercepted method
+     */
     public Object logAround(ProceedingJoinPoint joinPoint, LogAround logAround)  throws Throwable {
 
         Logger logger = LoggerUtil.getLogger(logAround.declaringClass(), joinPoint);
@@ -67,6 +112,19 @@ public class LogAroundService {
 
     }
 
+    /**
+     * Logs the entering message before method execution.
+     * <p>
+     * This method is called before the intercepted method is executed. It registers
+     * the necessary string suppliers for the join point and generates the entering message
+     * using the appropriate template. If the method is configured to be ignored or if
+     * logging is disabled for the determined level, no logging will occur.
+     *
+     * @param joinPoint The ProceedingJoinPoint representing the intercepted method
+     * @param logAround The LogAround annotation containing logging configuration
+     * @param logger The logger to use for logging
+     * @param stringLookup The string lookup for variable interpolation
+     */
     private void logEnteringMessage(
             ProceedingJoinPoint joinPoint,
             LogAround logAround,
@@ -87,6 +145,20 @@ public class LogAroundService {
         LoggerUtil.log(logger, loggingLevel, enteringMessage);
     }
 
+    /**
+     * Logs the exited message after successful method execution.
+     * <p>
+     * This method is called after the intercepted method has executed successfully.
+     * It registers the necessary string suppliers for the return value and generates
+     * the exited message using the appropriate template. If the method is configured
+     * to be ignored or if logging is disabled for the determined level, no logging will occur.
+     *
+     * @param joinPoint The ProceedingJoinPoint representing the intercepted method
+     * @param logAround The LogAround annotation containing logging configuration
+     * @param logger The logger to use for logging
+     * @param stringLookup The string lookup for variable interpolation
+     * @param returnValue The value returned by the intercepted method
+     */
     private void logExitedMessage(
             ProceedingJoinPoint joinPoint,
             LogAround logAround,
@@ -108,6 +180,24 @@ public class LogAroundService {
         LoggerUtil.log(logger, loggingLevel, exitedMessage);
     }
 
+    /**
+     * Logs the exited abnormally message when the method throws an exception.
+     * <p>
+     * This method is called when the intercepted method has thrown an exception.
+     * It registers the necessary string suppliers for the exception and generates
+     * the exited abnormally message using the appropriate template. If the method is
+     * configured to be ignored, if logging is disabled for the determined level, or if
+     * the exception is configured to be ignored, no logging will occur.
+     * <p>
+     * If the printStackTrace flag is set in the annotation, the exception stack trace
+     * will be included in the log message.
+     *
+     * @param logAround The LogAround annotation containing logging configuration
+     * @param logger The logger to use for logging
+     * @param stringLookup The string lookup for variable interpolation
+     * @param exception The exception thrown by the intercepted method
+     * @param joinPoint The ProceedingJoinPoint representing the intercepted method
+     */
     private void logExitedAbnoramllyMessage(
             LogAround logAround,
             Logger logger,
@@ -134,6 +224,21 @@ public class LogAroundService {
         }
     }
 
+    /**
+     * Logs the elapsed time for method execution.
+     * <p>
+     * This method is called after the intercepted method has executed (either successfully
+     * or with an exception). It registers the necessary string suppliers for the elapsed time
+     * and generates the elapsed time message using the appropriate template. If the method is
+     * configured to be ignored or if logging is disabled for the determined level, no logging
+     * will occur.
+     *
+     * @param logAround The LogAround annotation containing logging configuration
+     * @param logger The logger to use for logging
+     * @param stringLookup The string lookup for variable interpolation
+     * @param endTime The elapsed time in nanoseconds
+     * @param joinPoint The ProceedingJoinPoint representing the intercepted method
+     */
     private void logElapsedTime(
             LogAround logAround,
             Logger logger,
@@ -154,14 +259,40 @@ public class LogAroundService {
         LoggerUtil.log(logger, loggingLevel, elapsedTimeMessage);
     }
 
+    /**
+     * Determines the message template to use based on annotation and properties.
+     * <p>
+     * If the annotation provides a non-empty template, it will be used. Otherwise,
+     * the default template from properties will be used.
+     *
+     * @param annotationTemplate The template specified in the annotation
+     * @param propertiesTemplate The default template from properties
+     * @return The message template to use
+     */
     private String getMessageTemplate(String annotationTemplate, String propertiesTemplate) {
         return annotationTemplate.isEmpty() ? propertiesTemplate : annotationTemplate;
     }
 
+    /**
+     * Checks if logging is disabled for the specified level.
+     *
+     * @param logger The logger to check
+     * @param annotationLevel The logging level to check
+     * @return true if logging is disabled, false otherwise
+     */
     private boolean isLoggingLevelDisabled(Logger logger, Level annotationLevel) {
         return !LoggerUtil.isEnabled(logger, annotationLevel);
     }
 
+    /**
+     * Determines the appropriate logging level based on annotation.
+     * <p>
+     * If the annotation specifies a non-default level, it will be used. Otherwise,
+     * the default entering level from properties will be used.
+     *
+     * @param annotationLevel The level specified in the annotation
+     * @return The logging level to use
+     */
     private Level getLoggingLevel(Level annotationLevel) {
         return annotationLevel == Level.DEFAULT ? aopLoggersProperties.getEnteringLevel() : annotationLevel;
     }
